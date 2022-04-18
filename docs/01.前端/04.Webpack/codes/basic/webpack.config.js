d38c7bf9a01d8c53e023cb77ff22434cbd64e94e
lemon <1041707577@qq.com>
2022-04-18 20:50:07 +0800

    更新

--- docs/01.前端/04.Webpack/codes/basic/webpack.config.js
+++ docs/01.前端/04.Webpack/codes/basic/webpack.config.js
@@ -1,12 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
-  entry: './main.js',
+  mode: 'development', // 开发环境
+  entry: './main.js', // 入口文件
   output: {
-    filename: 'bundle.js'
+    filename: 'bundle.js' // 输出文件
   },
   plugins : [
-    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin(), // 
   ],
 };
