f786cb2774772e86aa58497976f36eca97da4901
lemon <1041707577@qq.com>
2022-05-08 10:23:38 +0800

    feat: useReducer

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -246,7 +246,15 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
 
     // 全文搜索。 ⚠️注意：此插件会在打开网站时多加载部分js文件用于搜索，导致初次访问网站变慢。如在意初次访问速度的话可以不使用此插件！（推荐：vuepress-plugin-thirdparty-search）
     'fulltext-search',
-
+    'ribbon',
+    'cursor-effects',
+    // [
+    //   'sitemap',
+    //   {
+    //     hostname: 'https://pake.web.id'
+    //   },
+    // ]
+    
     // 可以添加第三方搜索链接的搜索框（继承原官方搜索框的配置参数）
     // 'thirdparty-search': {
     //   thirdparty: [
@@ -275,13 +283,13 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     // },
 
     [
-      'one-click-copy', // 代码块复制按钮
-      {
-        copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
-        copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
-        duration: 1000, // prompt message display time.
-        showInMobile: false, // whether to display on the mobile side, default: false.
-      },
+    'one-click-copy', // 代码块复制按钮
+    {
+      copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
+      copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+      duration: 1000, // prompt message display time.
+      showInMobile: false, // whether to display on the mobile side, default: false.
+    },
     ],
 
     [
