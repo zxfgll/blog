0bce8475f5e932cf2d1648bb5017daa4d2e1679a
linyuchen <Lyc1688!!>
2022-05-26 17:32:49 +0800

    feat: pinia-store

--- codes/pinia-demo/src/components/count.vue
+++ codes/pinia-demo/src/components/count.vue
@@ -2,6 +2,7 @@
     <div>
         {{num}}
         <button @click="add">add</button>
+        <button @click="reset">reset</button>
     </div>
 </template>
 
@@ -16,7 +17,14 @@ const {num} = storeToRefs(counter) // 丢失响应性
 const add = () =>{
     // counter.num ++ // 直接操作state
     // counter.$patch({num : counter.num + 1}) // 定义一个局部state，增量更新源state
+    // counter.$patch((state)=>{ // 如果你为patch传入了一个函数，pinia会提供state作为参数供你操作，从vuex开始就已经引入了immer，所以放心大胆的直接操作对象和数组吧
+    //     state.num = state.num + 1
+    // })
     counter.increment() // 调用方法
+}
+
+const reset = () =>{
+    counter.$reset()
 }
 
 </script>
