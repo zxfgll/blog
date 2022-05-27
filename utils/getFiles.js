034315b853b318813750f0d0aa98a61553f9736b
linyuchen <Lyc1688!!>
2022-05-27 17:58:01 +0800

    feat: js-迭代器生成器

--- utils/getFiles.js
+++ utils/getFiles.js
@@ -2,37 +2,43 @@ const fs = require('fs'); // 文件模块
 const path = require('path'); // 路径模块
 const docsRoot = path.join(__dirname, '..', 'docs'); // docs文件路径
 
-// class Node {
-//     constructor(path){
-//         this.name = path.win32.basename
-//     }
+class Node {
+    constructor(path) {
+        // this.name = path.win32.basename
+    }
+}
+
+class FileMap {
+    constructor() {
+        this.Map = new Map()
+        this.root = fs.opendirSync(docsRoot)
+    }
+
+    readFileList = async (dir = this.root, parent = null) => {
+        
+        // for await (const dirent of dir){
+        //     console.log(dirent);
+        // }
+    }
+
+    run = () => {
+        this.readFileList()
+    }
+}
+
+const fileMap = new FileMap()
+
+fileMap.readFileList()
+
+
+
+// function readFileList2(dir = docsRoot) {
+//     const files = fs.readdirSync(dir, { withFileTypes: true })
+
+//     console.log(files);
 // }
 
-const fileMap = new Map()
-
-function getDirName(filePath) {
-    const fileNameArr = path.basename(filePath).split('.')
-    if (fileNameArr.length === 2) {
-        return log(chalk.yellow(`warning: 该文件 "${filePath}" 没有按照约定命名，将忽略生成相应数据。`))
-    }
-    return fileNameArr[1]
-}
-
-function getFileName(filePath){
-    const fileNameArr = path.basename(filePath).split('.')
-    let name = null, type = null;
-    if (fileNameArr.length === 2) { // 没有序号的文件
-        name = fileNameArr[0]
-        type = fileNameArr[1]
-    } else if (fileNameArr.length === 3) { // 有序号的文件
-        name = fileNameArr[1]
-        type = fileNameArr[2]
-    } else { // 超过两个‘.’的
-        log(chalk.yellow(`warning: 该文件 "${filePath}" 没有按照约定命名，将忽略生成相应数据。`))
-        return
-    }
-    return {name , type}
-}
+// readFileList2()
 
 function readFileList(dir = docsRoot, filesList = []) {
     const files = fs.readdirSync(dir);
@@ -42,7 +48,7 @@ function readFileList(dir = docsRoot, filesList = []) {
         const stat = fs.statSync(filePath);
 
         if (stat.isDirectory() && item !== '.vuepress') { //递归读取文件
-            
+
             return readFileList(path.join(dir, item), filesList)
         }
 
