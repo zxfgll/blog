28fc88044ba09ca5688d021854d5ca3a685ce5fd
lemon <1041707577@qq.com>
2022-05-29 21:58:27 +0800

    feat: 生成目录文件

--- utils/getFiles.js
+++ utils/getFiles.js
@@ -1,120 +1,217 @@
-const { opendirSync } = require('fs');
-const fs = require('fs'); // 文件模块
+const { opendirSync, accessSync, constants } = require('fs');
 const path = require('path'); // 路径模块
+const moment = require('moment');
+const { mkdirSync } = require('fs');
+const { writeFileSync } = require('fs');
 const docsRoot = path.join(__dirname, '..', 'docs'); // docs文件路径
-
+const catalogueName = '00.目录页'
 
 class File {
     /**
      * 
-     * @param {*} dirent dirent对象 
-     * @param {*} parent Map对象
+     * @param {*} dirent 文件实体 
+     * @param {*} parent 父文件夹Map字典
      */
-    constructor(dirent , parent) {
+    constructor(dirent, parent) {
         this.name = dirent.name
         this.entity = dirent
-        this.parent = parent
+
+        this.parent = parent ? parent.get('root') : null
+        this.depth = parent ? parent.get('root').depth + 1 : 0
     }
 }
 
+class Directory {
+    /**
+     * 
+     * @param {*} dir 文件夹实体 
+     * @param {*} _Map 文件夹Map字典
+     * @param {*} parent 父文件夹Map字典
+     */
+    constructor(dir, _Map, parent) {
+        this.name = path.basename(dir.path)
+        this.path = dir.path
+        this.entity = dir
+        this._Map = _Map
+        this.parent = parent ? parent.get('root') : null
+        this.depth = parent ? parent.get('root').depth + 1 : 0
+    }
+}
+
+/**
+ * 实现方法：
+ * 1.   获取yaml头部
+ * 2.   实现文件夹的深度数据
+ * 3.   导航栏类
+ */
+
+/**
+ * 预处理：
+ * 如果有第三级目录 
+ *      1.  在00.目录页下 创建第二级目录文件夹
+ *      2.  将第三级目录名作为link生成Catalogue的markdown文件，存放在第二季目录文件夹下 
+ * 
+ * 生成导航栏
+ *  1. 读取00.目录页， 文件夹名作为text，文件名作为items，读取文件获取Link
+ *  2. 读取FileMap对象，如果没有第三级目录 
+ *      1. 用第二级目录名查找导航栏对象text，若有，push。若无，新建。
+ *      2. 父级文件夹名作为text ，文件名作为items，读取文件获取link。 
+ */
+
 class FileMap {
-    constructor() {
-        this.root = opendirSync(docsRoot)
-        this.dictory = new Map()
-        this.dictory.set('root' , this.root)
+    constructor(docsRoot) {
+        const root = opendirSync(docsRoot)
+        this.dictionary = new Map()
+        const directory = new Directory(root, this.dictionary, null)
+
+        this.dictionary.set('root', directory) // root保存着当前文件夹的信息
     }
 
-    readFileList = async (dir = this.root, parent = this.dictory) => {
-        for await (const dirent of dir){
-            if(dirent.isDirectory()){
-                const dictory_path = path.join(dir.path , dirent.name)
-                const new_root = opendirSync(dictory_path)
-                const new_dictory = new Map()
-                new_dictory.set('root' , new_root)
+    /**
+     * 
+     * @param {*} dir 父文件夹实体 
+     * @param {*} _Map 父文件夹Map字典
+     */
+    readFileList = async (dir = this.dictionary.get('root').entity, _Map = this.dictionary) => {
+        for await (const dirent of dir) {
+            if (dirent.isDirectory()) {
+                const dictory_path = path.join(dir.path, dirent.name)
 
-                parent.set(dirent.name , new_dictory)
-                this.readFileList(new_root , new_dictory)
-            }else{
-                const file = new File(dirent , parent)
-                parent.set(dirent.name , file)
+                const root = opendirSync(dictory_path) // 子文件夹的实体
+                const dictionary = new Map() // 子文件夹的map
+                const directory = new Directory(root, dictionary, _Map) // 子文件夹的类
+                dictionary.set('root', directory)
+
+                _Map.set(dirent.name, dictionary) // 子文件夹信息的key值为文件名，类型为 Directory
+
+                this.readFileList(root, dictionary)
+            } else {
+                const file = new File(dirent, _Map)
+                _Map.set(dirent.name, file) // 子文件信息的key值为文件名，类型为File
             }
         }
     }
 
-    run = () => {
-        this.readFileList()
-    }
+    getListWithDepth = (depth) => {
+        let list = []
 
-    test2  = () =>{
-        return new Promise((resolve , reject)=>{
-            setTimeout(()=>{
-                resolve(1)
-            },1000)
-        })
-    }
+        /**
+         * 
+         * @param {*} dictionary Map字典 
+         */
+        const search = (dictionary = this.dictionary) => {
+            if (dictionary.constructor === File) return
+            for (let [key, value] of dictionary) {
+                if (key === 'root') {
+                    if (value.depth === depth) {
+                        list.push(value)
+                    }
+                } else {
+                    search(value)
+                }
+            }
+        }
 
-    test = async() =>{
-        this.asd = await this.test2()
+        search()
+
+        return list
     }
 }
 
-const fileMap = new FileMap()
-
-// fileMap.readFileList()
-
-async function getRes () {
-    await fileMap.readFileList()
-    console.log(111);
+class NavItem {
+    constructor(path, fileName) {
+        this.key = path
+        this.description = fileName
+        this.permalink = `/${fileName}/`
+        this.title = fileName
+        this.date = moment().format('YYYY-MM-DD HH:mm:ss')
+        // this.name = 'Catalogue'
+    }
 }
 
-getRes()
+class Nav {
+    constructor(rootPath, catalogueName) {
+        this.rootPath = rootPath
+        this.cataloguePath = path.join(rootPath, catalogueName)
+    }
 
+    async generateCatalogueFile() {
+        const generateCatalogueMap = async () => {
+            const fileMap = new FileMap(this.rootPath)
+            await fileMap.readFileList()
+            const list = fileMap.getListWithDepth(3)
 
-// function readFileList2(dir = docsRoot) {
-//     const files = fs.readdirSync(dir, { withFileTypes: true })
+            const dir = new Map()
 
-//     console.log(files);
-// }
+            for (const item of list) {
+                const reg = /\d+\.(.+)/
+                const parentName = item.parent.name
+                const fileName = item.name
+                const filePath = item.path
+                let dirPureName, filePureName, fileRelativePath
 
-// readFileList2()
+                if (!reg.test(parentName)) continue
+                if (!reg.test(fileName)) continue
 
-function readFileList(dir = docsRoot, filesList = []) {
-    const files = fs.readdirSync(dir);
+                dirPureName = reg.exec(parentName)[1]
+                filePureName = reg.exec(fileName)[1]
+                fileRelativePath = path.relative(this.rootPath, filePath)
 
-    for (item of files) {
-        let filePath = path.join(dir, item);
-        const stat = fs.statSync(filePath);
+                const navItem = new NavItem(fileRelativePath, filePureName)
 
-        if (stat.isDirectory() && item !== '.vuepress') { //递归读取文件
-
-            return readFileList(path.join(dir, item), filesList)
+                if (!dir.get(dirPureName)) {
+                    dir.set(dirPureName, [navItem])
+                } else {
+                    dir.get(dirPureName).push(navItem)
+                }
+            }
+            return dir
         }
 
-        if (path.basename(dir) === 'docs') return
+        const fileExist = (path) =>{
+            try {
+                accessSync(path, constants.F_OK);
+            } catch (err) {
+                return false
+            }
+            return true
+        } 
 
-        const fileNameArr = path.basename(filePath).split('.')
-        let name = null, type = null;
-        if (fileNameArr.length === 2) { // 没有序号的文件
-            name = fileNameArr[0]
-            type = fileNameArr[1]
-        } else if (fileNameArr.length === 3) { // 有序号的文件
-            name = fileNameArr[1]
-            type = fileNameArr[2]
-        } else { // 超过两个‘.’的
-            log(chalk.yellow(`warning: 该文件 "${filePath}" 没有按照约定命名，将忽略生成相应数据。`))
-            return
-        }
-        if (type === 'md') { // 过滤非md文件
-            filesList.push({
-                name,
-                filePath
-            });
+        const generateFile = (catalogueMap) => {
+            for (const [dirname, files] of catalogueMap) {
+                const dirPath = path.join(this.cataloguePath, dirname)
+                if(fileExist(dirPath)) continue
+
+                mkdirSync(dirPath)
+                
+                for(const file of files){
+                    const filePath = path.join(dirPath , `${file.title}.md`)
+                    
+                    if(fileExist(filePath)) continue
+                    const content = `---
+                    pageComponent: 
+                        name: Catalogue
+                        data:
+                            key:${file.key}
+                            description:${file.description}
+                    date : ${file.date}
+                    title: ${file.title}
+                    permalink: ${file.permalink}
+                    ---`
+                    
+                    writeFileSync(filePath , content)
+                }
+            }
         }
 
+        const catalogueMap = await generateCatalogueMap()
+        generateFile(catalogueMap)
+    }
+
+    getNavConfig() {
 
     }
-    return filesList;
 }
 
-
-//path.basename() 方法返回 path 的最后一部分
+const nav = new Nav(docsRoot, catalogueName)
+nav.generateCatalogueFile()
