c435cbd34f03e6a86128e09f86fe1f49be4e4e61
linyuchen <Lyc1688!!>
2022-05-30 15:50:22 +0800

    feat: pinia-插件

--- codes/pinia-demo/src/main.js
+++ codes/pinia-demo/src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import {createPinia} from 'pinia'
+import { createPinia } from 'pinia'
+import { debounce } from 'lodash'
 
-createApp(App).use(createPinia()).mount('#app')
+const pinia = createPinia()
+
+pinia.use(({ store, options }) => {
+    if (options.debounce) {
+        const actionsList = Object.keys(options.debounce) // 获取节流action列表
+        actionsList.forEach((action) => {
+            store[action] = debounce(store[action], options.debounce[action]) // action会被挂载到store的实例上，我们直接用节流后的action替代原来的action
+        })
+    }
+})
+
+createApp(App).use(pinia).mount('#app')
--- codes/pinia-demo/src/store/image.js
+++ codes/pinia-demo/src/store/image.js
@@ -1,12 +1,15 @@
-import {defineStore} from 'pinia'
+import { defineStore } from 'pinia'
 
-export const useImageStore = defineStore('image' , {
-    state : ()=>({imgUrl : ""}),
-    actions :{
-        async getImg(url){
-            const imgUrl = await fetch(url).then(res => res.json()).then(({images})=> images[0].url)
+export const useImageStore = defineStore('image', {
+    state: () => ({ imgUrl: "" }),
+    actions: {
+        async getImg(url) {
+            const imgUrl = await fetch(url).then(res => res.json()).then(({ images }) => images[0].url)
             this.imgUrl = imgUrl
             return imgUrl
         }
     },
+    debounce: { // 设置自定义属性给插件处理
+        getImg: 300,
+    }
 })
